3083c929982d786751a83b007933c9f9b223e0ca
Radosław Grzanka <radoslawg@gmail.com>
2025-02-01 21:27:52 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (    
+    <div className="App">
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Hello, World!</h1>
+      <p>Welcome to your React TypeScript project running in a dev container!</p>
+    </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
